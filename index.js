568c5171372ed827a80d7be54704a25a22975bd8
Nico-186 <rnicolastabilo@gmail.com>
2024-06-25 01:16:40 -0400

    cors fix

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
 const user = require("./routes/User");
 const login = require("./routes/login");
 
 const port = 3000;
 
-app.use(cors());
 app.use(express.json());
 
 app.use("/admin/allUsers/", user)
